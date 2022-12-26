5c3e6067bd50b0890a6c77c42b49bacd7d77d1c7
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-12-26 17:31:59 +0300

    init

---
+++ lib/huginn_ruby_agent.rb
@@ -0,0 +1,4 @@
+require 'huginn_agent'
+
+#HuginnAgent.load 'huginn_ruby_agent/concerns/my_agent_concern'
+HuginnAgent.register 'huginn_ruby_agent/ruby_agent'
---
+++ spec/ruby_agent_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+require 'huginn_agent/spec_helper'
+
+describe Agents::RubyAgent do
+  before(:each) do
+    @valid_options = Agents::RubyAgent.new.default_options
+    @checker = Agents::RubyAgent.new(:name => "RubyAgent", :options => @valid_options)
+    @checker.user = users(:bob)
+    @checker.save!
+  end
+
+  pending "add specs here"
+end
